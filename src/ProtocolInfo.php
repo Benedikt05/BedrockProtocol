d3e18a682ceb56660cba2f6f9017dca9fb5fff03
Benedikt05 <60869945+Benedikt05@users.noreply.github.com>
2026-08-29 14:45:36 +0200

    Update protocol and Minecraft version to 1.26.45

--- src/ProtocolInfo.php
+++ src/ProtocolInfo.php
@@ -32,11 +32,11 @@ final class ProtocolInfo{
 	 */
 
 	/** Actual Minecraft: PE protocol version */
-	public const CURRENT_PROTOCOL = 2168;
+	public const CURRENT_PROTOCOL = 2169;
 	/** Current Minecraft PE version reported by the server. This is usually the earliest currently supported version. */
-	public const MINECRAFT_VERSION = 'v1.26.40';
+	public const MINECRAFT_VERSION = 'v1.26.45';
 	/** Version number sent to clients in ping responses. */
-	public const MINECRAFT_VERSION_NETWORK = '1.26.40';
+	public const MINECRAFT_VERSION_NETWORK = '1.26.45';
 
 	public const LOGIN_PACKET = 0x01;
 	public const PLAY_STATUS_PACKET = 0x02;
